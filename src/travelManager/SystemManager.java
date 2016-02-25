01af60ce73a3b1469809b033e3314f1380d9791b
Greig <gm14793@Greig-McAllisters-MacBook-Pro.local>
2016-02-25 12:19:42 -0800

    Beginning work on the SystemManager and Airport classes

---
+++ src/travelManager/SystemManager.java
@@ -0,0 +1,17 @@
+package travelManager;
+
+import java.util.ArrayList;
+
+public class SystemManager{
+
+	public static void main(String[] args){
+		ArrayList<Airport> airports = new ArrayList<Airport>();
+	
+	
+	}
+	public Airport createAirport(String name){
+		return null;
+	}
+
+}
+
